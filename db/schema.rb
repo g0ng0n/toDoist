8cb8a9d918d5efcd2f155b112df30f1b48f1134d
Gonzalo <gonzalo.a.gisbert@gmail.com>
2015-01-31 12:54:53 -0300

    added link detection

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131030302) do
+ActiveRecord::Schema.define(version: 20150131155023) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "user_id"
-    t.string   "state",      default: "to_do"
+    t.string   "state",        default: "to_do"
+    t.string   "content_html"
+    t.string   "text"
   end
 
   create_table "users", force: :cascade do |t|
